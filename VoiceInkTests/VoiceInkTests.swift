ad27a5973a9c9f0f13c41b5c80b2499efcb56a68
Joe Li <li.joe@outlook.com>
2025-09-26 18:45:46 +0800

    init

---
+++ VoiceInkTests/VoiceInkTests.swift
@@ -0,0 +1,17 @@
+//
+//  JoeVoiceTests.swift
+//  JoeVoiceTests
+//
+//  Created by Prakash Joshi on 15/10/2024.
+//
+
+import Testing
+@testable import JoeVoice
+
+struct JoeVoiceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
